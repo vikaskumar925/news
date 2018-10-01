939e4ee697c2a59c03f17540365f34c1c7ff01ba
Vikas Kumar <negi.vikash925@gmail.com>
2018-10-01 19:59:58 +0530

    Remove the pipes

--- src/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CategoryIdToNamePipe } from './category-id-to-name/category-id-to-name';
-@NgModule({
-	declarations: [CategoryIdToNamePipe],
-	imports: [],
-	exports: [CategoryIdToNamePipe]
-})
-export class PipesModule {}
